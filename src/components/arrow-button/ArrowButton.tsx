b83facf4173736309cf8d772a5d00f1e7ab88ece
yurydzal <142845528+yurydzal@users.noreply.github.com>
2024-06-28 19:58:57 +0400

    review

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -5,15 +5,25 @@ import styles from './ArrowButton.module.scss';
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+type Props = {
+	openStatus?: () => void;
+	open?: boolean;
+};
+
+export const ArrowButton = ({ openStatus, open }: Props) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={styles.container + (open ? ` ${styles.container_open}` : '')}
+			onClick={openStatus}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={styles.arrow + (open ? ` ${styles.arrow_open}` : '')}
+			/>
 		</div>
 	);
 };
